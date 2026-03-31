27b4489572df10dd26e514415212537f72604876
fa1778 <fa1778@srmist.edu.in>
2026-03-31 20:57:13 +0530

    feature/UC1:Print OOPS in banner

--- HelloApp.java
+++ HelloApp.java
@@ -1,5 +1,7 @@
 class HelloApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        System.out.println("* * * *");
+        System.out.println("O O P S");
+        System.out.println("* * * *");
     }
 }
