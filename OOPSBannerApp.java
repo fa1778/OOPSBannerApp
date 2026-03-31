0d21c6d4d6dbecbe896e422ca7e8c2b417f97685
fa1778 <fa1778@srmist.edu.in>
2026-03-31 21:05:12 +0530

    feature/UC2: Print OOPS banner using *

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,19 @@
 /**
  * OOPSBannerApp
- * UC1: Print OOPS to Console
- * @author Student
- * @version 1.0
+ * UC2: Print OOPS Banner using *
  */
 
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        System.out.println(" ***     ***    *****    ***** ");
+        System.out.println("*   *   *   *   *   *    *     ");
+        System.out.println("*   *   *   *   *****    ****  ");
+        System.out.println("*   *   *   *   *        *     ");
+        System.out.println("*   *   *   *   *        *     ");
+        System.out.println("*   *   *   *   *        *     ");
+        System.out.println(" ***     ***    *        ***** ");
+
     }
 }
