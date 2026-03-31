27fc002afc09fcd1521b9c41854f2cd76fface9b
fa1778 <fa1778@srmist.edu.in>
2026-03-31 21:39:36 +0530

    Added UC5 using array initialization and String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,26 +1,15 @@
-/**
- * OOPSBannerApp
- * UC4: Print OOPS Banner using Array and Loop
- */
-
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
-
-        // Step 1: Create array
         String[] banner = {
-
-            String.join(" ", " *** ", " *** ", "*****", "*****"),
-            String.join(" ", "*   *", "*   *", "*   *", "*"),
-            String.join(" ", "*   *", "*   *", "*****", "****"),
-            String.join(" ", "*   *", "*   *", "*", "*"),
-            String.join(" ", "*   *", "*   *", "*", "*"),
-            String.join(" ", "*   *", "*   *", "*", "*"),
-            String.join(" ", " *** ", " *** ", "*", "*****")
-
+            String.join("  ", " ***** ", " ******", " ******", " ******"),
+            String.join("  ", "*     *", "*     *", "*     *", "*     *"),
+            String.join("  ", "*     *", "*     *", "*     *", "*     *"),
+            String.join("  ", "*     *", "****** ", "****** ", "****** "),
+            String.join("  ", "*     *", "*       ", "*       ", "*     *"),
+            String.join("  ", "*     *", "*       ", "*       ", "*     *"),
+            String.join("  ", " ***** ", "*       ", "*       ", "****** ")
         };
 
-        // Step 2: Loop and print
         for (String line : banner) {
             System.out.println(line);
         }
